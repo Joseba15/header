41211ac60aeae13ce2b3783c201c39842b95468c
Joseba15 <josebadg15@gmail.com>
2022-12-02 12:39:07 +0100

    Subido el menu hecho

--- WebsiteProject/script.js
+++ WebsiteProject/script.js
@@ -1,15 +1,30 @@
 window.onload = init;
+    // function init(){
+    //     document.querySelectorAll(".more")[0].addEventListener("click",submenu);
+    //     document.querySelector(".submenu > ul").style.display = "none"; 
+    // }
+    
+    // function submenu(){
+    //     var estado = document.querySelector(".submenu > ul").style.display;
+    //     if (estado == "none"){
+    //         document.querySelector(".submenu > ul").style.display = "block";
+    //     }else{
+    //         document.querySelector(".submenu > ul").style.display = "none";            
+    //     };
+        
+    // }
     function init(){
         document.querySelectorAll(".more")[0].addEventListener("click",submenu);
-        document.querySelector(".submenu > ul").style.display = "none"; 
+        
     }
     
     function submenu(){
-        var estado = document.querySelector(".submenu > ul").style.display;
-        if (estado == "none"){
-            document.querySelector(".submenu > ul").style.display = "block";
-        }else{
-            document.querySelector(".submenu > ul").style.display = "none";            
-        };
+        var estado = document.querySelector(".menuDiv").style.display;
+        // if (estado == "none"){
+        //     document.querySelector(".menuDiv").style.display = "block";
+        // }else{
+        //     document.querySelector(".menuDiv").style.display = "none";            
+        // };
+        document.querySelector(".menuDiv").classList.toggle('desplegado')
         
     }
