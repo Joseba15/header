8fdddec5cc2c8b47523bb16b8fb57909e8d0377b
Joseba15 <josebadg15@gmail.com>
2022-12-14 10:22:12 +0100

    Subido todo

---
+++ header/script.js
@@ -0,0 +1,31 @@
+window.onload = init;
+    // function init(){
+    //     document.querySelectorAll(".more")[0].addEventListener("click",submenu);
+    //     document.querySelector(".submenu > ul").style.display = "none"; 
+    // }
+    
+    // function submenu(){
+    //     var estado = document.querySelector(".submenu > ul").style.display;
+    //     if (estado == "none"){
+    //         document.querySelector(".submenu > ul").style.display = "block";
+    //     }else{
+    //         document.querySelector(".submenu > ul").style.display = "none";            
+    //     };
+        
+    // }
+    function init(){
+        document.querySelectorAll(".more")[0].addEventListener("click",submenu);
+        
+    }
+    
+    function submenu(){
+        var estado = document.querySelector(".menuDiv").style.display;
+        // if (estado == "none"){
+        //     document.querySelector(".menuDiv").style.display = "block";
+        // }else{
+        //     document.querySelector(".menuDiv").style.display = "none";            
+        // };
+        document.querySelector(".menuDiv").classList.toggle('desplegado')
+        // document.querySelector(".more").style.background-color=" #434343";
+        
+    }
